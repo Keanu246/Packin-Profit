abea3b9afb9912ba29e7148cfd1649cdc5991664
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-25 19:30:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ var holyboxes = 0;
 var packers = 0;
 
 function buyPacker(){
-    var cursorCost = Math.floor(200 * Math.pow(1.2,packers));     //works out the cost of this packer
+    var packerCost = Math.floor(200 * Math.pow(1.2,packers));     //works out the cost of this packer
     if(coins >= packerCost){                                   //checks that the player can afford the packer
         packers = packers + 1;                                   //increases number of packers
     	coins = coins - packerCost;                          //removes the coins spent
         document.getElementById('packers').innerHTML = packers;  //updates the number of packers for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of coins for the user
+	document.getElementById('packerCoins').innerHTML = packerCoins;  //updates the number of coins per second by each packer
     };
     var nextCost = Math.floor(200 * Math.pow(1.2,packers));       //works out the cost of the next packer
     document.getElementById('packerCost').innerHTML = nextCost;  //updates the packer cost for the user
@@ -23,6 +24,6 @@ function buyPacker(){
 
 window.setInterval(function(){
 	
-	coinClick(packers);
+	packerCoins(packers);
 	
 }, 1000);
