338efc2481012a3bec1da3942d17e001ef23f13a
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-15 20:44:28 +0100

    Update workers.js

--- workers.js
+++ workers.js
@@ -4,15 +4,16 @@ var robots = 0;
 var robotCoins = 50;
 
 function buyPacker(){
-  var packerCost = Math.floor(200 * Math.pow(1.10,packers));
+  var packerCost = Math.floor(100 * Math.pow(1.10,packers));
   if(coins >= packerCost){
      packers = packers + 1;
+     packerCoins = packerCoins + 1;
      coins = coins - packerCost;
      document.getElementById('packers').innerHTML = packers;
      document.getElementById('coins').innerHTML = coins;
      document.getElementById('packerCoins').innerHTML = packerCoins;
   };
-  var nextCost = Math.floor(200 * Math.pow(1.1,packers));
+  var nextCost = Math.floor(100 * Math.pow(1.1,packers));
   document.getElementById('packerCost').innerHTML = nextCost; 
 };
 
