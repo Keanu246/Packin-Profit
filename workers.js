b937cccb95b4a6c940134c0887a9e719ec8d1566
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-16 00:37:47 +0100

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,3 +1,97 @@
+class Worker
+{
+	constructor(name, productivity, price)
+	{
+		this.name = name;
+		this.level = 0;
+		this.price = price;
+		this.productivity = productivity;
+
+		// variables for displaying
+		// definition of areas
+		this.area = document.createElement("span");
+		this.area.id = name;
+
+		this.level_display = document.createElement("span");
+		this.level_display.id = this.name + "_level";
+
+		this.productivity_display = document.createElement("span");
+		this.productivity_display.id = this.name + "_productivity";
+
+		this.price_display = document.createElement("span");
+		this.price_display.id = this.name + "_price";
+
+		this.button = document.createElement("button");
+		this.button.innerHTML = "Upgrade";
+	        this.button.onclick = this.improve.bind(this);
+
+		// put together
+		this.area.append(document.createTextNode(name + " Level: "));
+		this.area.append(this.level_display);
+		this.area.append(document.createElement("br"));
+
+		this.area.append(document.createTextNode("Cookies per Second: "));
+		this.area.append(this.productivity_display);
+		this.area.append(document.createElement("br"));
+
+		this.area.append(document.createTextNode("Improvement Price: "));
+		this.area.append(this.price_display);
+		this.area.append(document.createElement("br"));
+
+		this.area.append(this.button);
+		this.area.append(document.createElement("br"));
+		this.area.append(document.createElement("br"));
+        
+        	setInterval(this.produce.bind(this), 1000);
+	}
+	
+	get_price() 
+	{
+		return (this.price / 2) * (this.level * this.level + 1) + (this.price / 2) * (this.level + 1)
+	}
+	
+	upgrade()
+	{
+        	if(cookies >= this.get_price())
+        	{
+        		cookies -= this.get_price();
+        		this.level += 1;
+        		altogether_productivity += this.productivity;
+        		this.renew_display();
+        		clicker.renew_display();
+        	}
+        	else
+        	{
+			alert("Not enough coins!");
+		}
+	}
+
+	renew_display()
+	{
+		this.level_display.innerHTML = this.level;
+		this.productivity_display.innerHTML = this.get_production_value();
+		this.price_display.innerHTML = this.get_price();
+	}
+
+
+	set_visible()
+	{
+		buildings.append(this.area);
+		this.renew_display();
+	}
+
+	produce()
+	{
+		cookies += this.get_production_value();
+		cookies_produced += this.get_production_value();
+	}
+	
+	get_production_value()
+	{
+		return this.level * this.productivity;
+	}
+}
+
 var workers = document.getElementById("workers");
 
 packer = new Worker("Packer", 1, 100);
