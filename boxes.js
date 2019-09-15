8afe0c245b632992cc97eb502ce53e8bd0435c5b
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-16 00:01:38 +0100

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -1,15 +1,14 @@
 var pizzabox = 0;
-var pizzaboxCoins = document.getElementById('coinClick');
 
 function buyPizzaBox(){
   var pizzaboxCost = Math.floor(50 * Math.pow(1.10,pizzaboxes));
   if(coins >= pizzaboxCost){
-     pizzaboxes = pizzaboxes + 1;
      coins = coins - pizzaboxCost;
-     coins = coins - pizzaboxCoins + 1;
+     pizzaboxes = pizzaboxes + 1;
+     coinClick = coinClick + 1;
      document.getElementById('pizzaboxes').innerHTML = pizzaboxes;
      document.getElementById('coins').innerHTML = coins;
-     document.getElementById('pizzaboxCoins').innerHTML = pizzaboxCoins;
+     document.getElementById('coinClick').innerHTML = coinClick;
   };
   var nextCost = Math.floor(50 * Math.pow(1.10,pizzaboxes));
   document.getElementById('pizzaboxCost').innerHTML = nextCost; 
