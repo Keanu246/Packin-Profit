e4547d2ee964c15f8cf513a74770057c2e810ed8
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-17 22:30:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var factoryTotal = 0;
 var factoryBPS = 400;
 var pizzaboxPrice = 50;
 var pizzaboxTotal = 0;
-var pizzaboxCPC = 0
+var pizzaboxCPC = 1
 
 function addCoins() {
 	coins = coins + clickStr;
