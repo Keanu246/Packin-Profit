50ab965ef37d35abacd4bd74a07ddbf8c660f401
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-17 23:05:55 +0100

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -7,6 +7,6 @@ function buyPizzaBox() { //function for buying more boxes
 		document.getElementById("pizzabox").innerHTML = 'Buy Pizza Box for ' + pizzaboxPrice + ' Coins';
 		document.getElementById("pizzaboxAmmount").innerHTML = 'You have ' + pizzaboxTotal + ' Pizza Boxes';
 		document.getElementById("pizzaboxProduce").innerHTML = 'Packing ' + (pizzaboxCPC * pizzaboxTotal).toFixed(1) + ' coins per click';
-		document.getElementById("clickStr").innerHTML = (pizzaboxCPC * pizzaboxTotal).toFixed(1)
+		document.getElementById("clickStr").innerHTML = (pizzaboxCPC).toFixed(1)
 	}
 }
