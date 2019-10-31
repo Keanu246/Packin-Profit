07342a83e3f8aa0d2e66121a66a00d7c6f66139d
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-10-31 19:05:36 +0000

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -1,1 +1,12 @@
+var packerUpgrade = false;
+var packerUpgrade2 = false;
 
+function checkUpgrades() {
+	console.log(packerUpgrade);
+	if(packerTotal >= 10 && packerUpgrade != true) {
+		var packerUpgradeElements = document.getElementsByClassName("packerUpgrade");
+		for (i = 0; i < packerUpgradeElements.length; i++){
+			packerUpgradeElements[i].style.display = "inline";
+		}
+	}
+}
